d209ec4b001e8639fdb48a3a0892a0f94df1d85b
Meng Le <mzhang@hotmail.co.uk>
2019-02-25 10:55:05 +0000

    First commit of the summary function for the inla model output: shows number of frontier and non-frontier borders after the routine.
    The computed neighbourhood matrix output has -1 for no neighbours, 0 for frontiers and 1 for non frontier bords.
    It should also be an upper triangle matrix (to avoid repeated stuff).
    
    The summary uses tabulate instead of table because table is slow (it converts integers to factors).

--- Source/summary_frontier_model.R
+++ Source/summary_frontier_model.R
@@ -1,9 +1,23 @@
 summary.frontier_model <- function(object, ...){
   ## Display the numbers of frontiers
+  borders.tab <- tabulate(object$W.estimated_cleaned + 1) # so table will convert integer to
+  #factor first so the quicker way is to use tabulate which will only display
+  ##  coutners of positive intergers
+  
+  print('Total N. of borders')
+  print(sum(borders.tab))
+  print('N. of borders: non-frontier vs frontier')
+  print(borders.tab) 
+  
   
 }
 
-?summary
+summary(mod.inla)
+(mod.inla$W.estimated_cleaned + 1) %>% tabulate
+mod.inla$W.estimated_cleaned %>% str # huge matrid
+
+
+
 
 ##  We justed need the outputed model matrix
 
