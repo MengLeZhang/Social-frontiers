e2af975dabab37fd1dc95c4fdc84117b9c46b21d
Meng Zhang <sa_Mi1mz@SHEFUNIAD.SHEF.AC.UK>
2019-03-06 13:18:55 +0000

    Finally got a quicker implementation.
    We first use parallel computing toget intersections only in a list
    
    Then we use do.call(c, list) for a quick concate of the list object.

--- Source/Meng's try at binding borders TEMP FILE.R
+++ Source/Meng's try at binding borders TEMP FILE.R
@@ -8,7 +8,7 @@ library(doParallel)
 ## Assume we have a objects called data, w.index and mod.inla
 c('data', 'w.index', 'mod.inla') %in% ls() ## should all be true
 ## Do it parallel?
-
+##  See the frontier creation source
 
 
 ##  Step 1: Create a empty sf file with what we want
@@ -16,20 +16,6 @@ data.for.borders <-
   data %>%
   mutate(phi = mod.inla$phi[['Median']]) %>%
   select(LSOA, percentunemployed, crimesperperson, phi)
-
-##  Take 1 intersection to get names off
-fornames <- 
-  data.for.borders[w.index$col[1],] %>% 
-  st_intersection(data.for.borders[w.index$col[1],]) # now we are intersecting polys to get borders
-
-#Create df of correct size with those names
-borders.sf <- data.frame(1:ncol(fornames) %>%
-                           rep(nrow(w.index)) %>% matrix(ncol = ncol(fornames)))
-names(borders.sf) <- names(fornames)
-
-##  Make back into sf
-borders.sf <- st_as_sf(borders.sf, 
-                       geometry = st_sfc(lapply(1:nrow(w.index), function(x) st_geometrycollection())))
 
 
 ##  Step 2a: Do a for loop for the intersection
@@ -39,7 +25,7 @@ registerDoParallel(detectCores() - 1) #use all but 1
 ## on vm its 5
 
 x <- proc.time()
-
+##  In parallel just extract intersections
 saved.sf <- 
   foreach (i=1:nrow(w.index) #,.combine = rbind
            ) %dopar% {
@@ -51,26 +37,40 @@ saved.sf <-
   zone1 <- w.index$col[i]
   zone2 <- w.index$row[i]
   
-  ##  Some reason doesn't work -- in parallel but does in single --
-  # borders.sf[i,] <- data.for.borders[zone1,] %>% 
-  #   st_intersection(data.for.borders[zone2,]) # now we are intersecting polys to get borders
-  #borders.sf$frontier[i] <- w.index$frontier[i]
-  
-  data.for.borders[zone1,] %>% 
+  temp <- data.for.borders[zone1,] %>% 
     st_intersection(data.for.borders[zone2,]) # now we are intersecting polys to get borders
+  
+  st_geometry(temp)
   }
 
 proc.time() - x
 stopImplicitCluster()
-#
+# saved.sf is a list
 
+
+##  Then use c not rbind in do.call
+system.time(
+  test <- do.call(c, saved.sf)
+)
+test %>% head
+test[1:100] %>% plot
+
+##  Done!
+
+
+##  Other test results -- using worse methods
 ## http://www.win-vector.com/blog/2015/07/efficient-accumulation-in-r/ ## so best would be to assign?
 ##  Results:
 ##  SMI3 (6 cores/ 6 threads: 2.00ghz) 
-##  No parallel: 5399 seconds - 89 minutes -- 
-##  5 threads: 3406 sec -- 56 min --- faster but not huge (w/ .combine = rbind)
-##  5 thread (same as above but no rbind): 88 sec!!! So huge -- now we have a list
+##  No parallel: 5399 seconds - 89 minutes -- old code using rbind
+##  5 threads: 3406 sec -- 56 min --- old code in paralle faster but not huge (w/ .combine = rbind)
 
-borders.sf %>% head
+##  Indexing :::
+# x <- proc.time()
+# for (i in 1:nrow(w.index)){
+#   borders.sf[['geometry']][i] <- saved.sf[[i]]$geometry
+# }
+# proc.time() - x
+##  Let's see how long this takes
+##  List : 4484 -- so ages
 
-?foreach
